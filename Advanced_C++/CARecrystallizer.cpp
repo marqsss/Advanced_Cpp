c5259b0bb7a71e29009bedb7e11e1dfdf53ca76f
Marek Pyzik <marku.b@interia.pl>
2019-06-06 22:25:44 +0200

    CARecrystallizer infdev

--- Advanced_C++/CARecrystallizer.cpp
+++ Advanced_C++/CARecrystallizer.cpp
@@ -49,7 +49,7 @@ void CARecrystallizer::recrystallize(double x, bool save, std::string path)
 	{
 
 	}
-	auto i = 0;
+	auto turn = 0;
 	for (auto t = 0; t < total_time; t += total_time / 1000)
 	{
 		// calculate deltaRo and distribute the uniform part
@@ -80,16 +80,140 @@ void CARecrystallizer::recrystallize(double x, bool save, std::string path)
 		for (auto&[a, b, e] : edges)
 			if (cellMap.at(a).at(b).dislocation_density > critical)
 			{
-				cellMap.at(a).at(b).recrystallized = i;
+				cellMap.at(a).at(b).recrystallized = turn;
 				cellMap.at(a).at(b).dislocation_density = 0;
 				updateCell(a, b, sf::Color(palette == 0 ? dice() % 255 : 0, palette == 1 ? dice() % 255 : 0, palette == 2 ? dice() % 255 : 0));
 			}
 		// grow existing nuclei
-		// double for with indexes, check neighbours for recrystallized==i-1 && this cell has max density of neighbours
+		auto check = [&](auto k, auto l, auto x, auto y) -> bool {
+			return cellMap.at(k).at(l).dislocation_density < cellMap.at(x).at(y).dislocation_density;
+		};
+		for (auto i = 0; i < cellMap.size(); ++i)
+			for (auto j = 0; j < cellMap.at(i).size(); ++j)
+			{
+				// do not check those already recrystallized
+				if (cellMap.at(i).at(j).recrystallized)
+					continue;
+				bool highest = true;
+				auto recrystNeighb_x = 0, recrystNeighb_y = 0;
+				if (boundaryCondition)
+				{
+					// left
+					if ((n != Pentagonal_Right))
+					{
+						if (check(i, j, (i + cellMap.size() - 1) % cellMap.size(), j))
+							highest = false;
+						if (cellMap.at((i - 1) % cellMap.size()).at(j).recrystallized == turn - 1)
+						{
+							recrystNeighb_x = (i - 1) % cellMap.size();
+							recrystNeighb_y = j;
+						}
+					}
+					// top
+					if ((n != Pentagonal_Bottom) && check(i, j, i, (j + cellMap.at(i).size() - 1) % cellMap.at(i).size()))
+					{
+						edges.emplace_back(i, j, false);
+						continue;
+					}
+					// right
+					if ((n != Pentagonal_Left) && check(i, j, (i + 1) % cellMap.size(), j))
+					{
+						edges.emplace_back(i, j, false);
+						continue;
+					}
+					// bottom
+					if ((n != Pentagonal_Top) && check(i, j, i, (j + 1) % cellMap.at(i).size()))
+					{
+						edges.emplace_back(i, j, false);
+						continue;
+					}
+					// top-right
+					if (n == Moore || n == Hexagonal_Right || n == Pentagonal_Top || n == Pentagonal_Right &&
+						check(i, j, (i + 1) % cellMap.size(), (j + cellMap.at(i).size() - 1) % cellMap.at(i).size()))
+					{
+						edges.emplace_back(i, j, false);
+						continue;
+					}
+					// bottom-right
+					if (n == Moore || n == Hexagonal_Left || n == Pentagonal_Right || n == Pentagonal_Bottom &&
+						check(i, j, (i + 1) % cellMap.size(), (j + 1) % cellMap.at(i).size()))
+					{
+						edges.emplace_back(i, j, false);
+						continue;
+					}
+					// bottom-left
+					if (n == Moore || n == Hexagonal_Right || n == Pentagonal_Bottom || n == Pentagonal_Left &&
+						check(i, j, (i + cellMap.size() - 1) % cellMap.size(), (j + 1) % cellMap.at(i).size()))
+					{
+						edges.emplace_back(i, j, false);
+						continue;
+					}
+					// top-left
+					if (n == Moore || n == Hexagonal_Left || n == Pentagonal_Top || n == Pentagonal_Left &&
+						check(i, j, (i + cellMap.size() - 1) % cellMap.size(), (j + cellMap.at(i).size() - 1) % cellMap.at(i).size()))
+					{
+						edges.emplace_back(i, j, false);
+						continue;
+					}
+				}
+				else
+				{
+					// left
+					if (i > 0 && n != Pentagonal_Right && check(i, j, i - 1, j))
+					{
+						edges.emplace_back(i, j, false);
+						continue;
+					}
+					// top
+					if (j > 0 && n != Pentagonal_Bottom && check(i, j, i, j - 1))
+					{
+						edges.emplace_back(i, j, false);
+						continue;
+					}
+					// right
+					if (i < cellMap.size() - 1 && n != Pentagonal_Left && check(i, j, i + 1, j))
+					{
+						edges.emplace_back(i, j, false);
+						continue;
+					}
+					// bottom
+					if (j < cellMap.at(i).size() - 1 && n != Pentagonal_Top && check(i, j, i, j + 1))
+					{
+						edges.emplace_back(i, j, false);
+						continue;
+					}
+					// top-right
+					if (j > 0 && i < cellMap.size() - 1 && (n == Moore || n == Hexagonal_Right || n == Pentagonal_Top || n == Pentagonal_Right) && check(i, j, i + 1, j - 1))
+					{
+						edges.emplace_back(i, j, false);
+						continue;
+					}
+					// bottom-right
+					if (i < cellMap.size() - 1 && j < cellMap.at(i).size() - 1 && n == Moore || n == Hexagonal_Left || n == Pentagonal_Right || n == Pentagonal_Bottom && check(i, j, i + 1, j + 1))
+					{
+						edges.emplace_back(i, j, false);
+						continue;
+					}
+					// bottom-left
+					if (i > 0 && j < cellMap.at(i).size() - 1 && n == Moore || n == Hexagonal_Right || n == Pentagonal_Bottom || n == Pentagonal_Left && check(i, j, i - 1, j + 1))
+					{
+						edges.emplace_back(i, j, false);
+						continue;
+					}
+					// top-left
+					if (i > 0 && j > 0 && n == Moore || n == Hexagonal_Left || n == Pentagonal_Top || n == Pentagonal_Left && check(i, j, i - 1, j - 1))
+					{
+						edges.emplace_back(i, j, false);
+						continue;
+					}
+				}
+				if (highest && recrystNeighb_x)
+					updateCell(i, j, cellMap.at(recrystNeighb_x).at(recrystNeighb_y).color);
+			}
 		/*t
 		getRo(t)
 		getSigma(getRo(t))*/
-		++i;
+		++turn;
 	}
 }
 
